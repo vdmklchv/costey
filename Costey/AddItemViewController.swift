8917a38011eb790b0584d3c4e4580f89e07f69cd
Vadim Colcev <vadimkolchev@gmail.com>
2020-12-15 00:03:31 +0200

    iteration7

--- Costey/AddItemViewController.swift
+++ Costey/AddItemViewController.swift
@@ -23,10 +23,6 @@ class AddItemViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-    }
-    
     @IBAction func saveButtonTapped(_ sender: Any) {
         let name = addItemTextField[0].text
         let price = addItemTextField[1].text
@@ -34,14 +30,15 @@ class AddItemViewController: UIViewController {
         let month = addItemTextField[3].text
         let year = addItemTextField[4].text
         
-        
         if let day = day, let month = month, let year = year, let price = price, let name = name {
+
             if let date = getDateObject(for: Int(day), month: Int(month), year: Int(year)) {
 
                 if let price = Double(price) {
 
                     let item = Item(name: name, price: price, startDate: date)
                     if delegate != nil {
+
                         let dataToSend = item
                         self.delegate?.sendData(myData: dataToSend)
                         self.delegate?.updateTable()
