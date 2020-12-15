d7afec679e3272960c2068e89a94dc9e753ad5fd
Vadim Colcev <vadimkolchev@gmail.com>
2020-12-15 16:05:21 +0200

    refactored code

--- Costey/AddItemViewController.swift
+++ Costey/AddItemViewController.swift
@@ -17,11 +17,6 @@ class AddItemViewController: UIViewController {
     var delegate: DataSendProtocol? = nil
 
     @IBOutlet var addItemTextField: [UITextField]!
-        
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
     
     @IBAction func saveButtonTapped(_ sender: Any) {
         let name = addItemTextField[0].text
@@ -32,14 +27,13 @@ class AddItemViewController: UIViewController {
         
         if let day = day, let month = month, let year = year, let price = price, let name = name {
 
-            if let date = getDateObject(for: Int(day), month: Int(month), year: Int(year)) {
+            if let date = Item.getDateObject(for: Int(day), month: Int(month), year: Int(year)) {
 
                 if let price = Double(price) {
 
                     let item = Item(name: name, price: price, startDate: date)
                     
                     if delegate != nil {
-                        print("Delegate is not nil")
                         let dataToSend = item
                         self.delegate?.sendData(myData: dataToSend)
                         self.delegate?.updateTable()
@@ -50,20 +44,5 @@ class AddItemViewController: UIViewController {
         }
     }
         
-}
-    
-    func getDateObject(for day: Int?, month: Int?, year: Int?) -> Date? {
-        var components = DateComponents()
-        components.year = year
-        components.month = month
-        components.day = day
-        if let date = Calendar.current.date(from: components) {
-            return date
-        }
-        else {
-            return nil
-        }
-    
-    
 }
 
--- Costey/MainScreenController.swift
+++ Costey/MainScreenController.swift
@@ -7,20 +7,9 @@
 
 import UIKit
 
-
-
 class MainScreenController: UITableViewController, DataSendProtocol {
     
     var currentItems: [Item] = []
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-    }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return currentItems.count
@@ -49,7 +38,6 @@ class MainScreenController: UITableViewController, DataSendProtocol {
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "getDataSegue" {
-            print("I'm here")
             let addItemVC: AddItemViewController = segue.destination as! AddItemViewController
             addItemVC.delegate = self
         }
